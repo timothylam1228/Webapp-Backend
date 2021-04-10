ee7e10707c1a2b558498a73414997a61acbf76b8
Timothy Lam <timothylam1228@gmail.com>
2021-04-11 02:30:01 +0800

    Auto stash before merge of "main" and "origin/main"

--- app.js
+++ app.js
@@ -1,24 +1,3 @@
-<<<<<<< Updated upstream
-// async function main() {
-
-//   const MongoClient = require('mongodb').MongoClient;
-//   const uri = "mongodb+srv://admin:JE1BPKXM0KnAYbYy@cluster1.u7wxz.mongodb.net/User?retryWrites=true&w=majority";
-//   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-//   try {
-//     await client.connect();
-
-//     await listDatabases(client);
-
-//   } catch (err) {
-//     console.error(err);
-//   } finally {
-//     client.close();
-//   }
-// }
-
-// main().catch(console.error);
-=======
 const express = require('express')
 const app = express()
 const port = 3001
@@ -30,4 +9,3 @@ app.get('/api', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
->>>>>>> Stashed changes

e90957b5ed95bf5bc28f7dbaf8647b62e22553f4
kaking <kakingchow33@gmail.com>
2021-04-12 03:13:18 +0800

    established insertion

--- handler.js
+++ handler.js
@@ -7,11 +7,22 @@ const register = require('./controller/register')
 
 
 module.exports.create = async (events) => {
-  const db = await connectToDB();
+  const db = await connectToDB.connectToDB();
   const collection = await db.collection("User");
-  const users = await collection.find({}).toArray();
-  res.status(200).json({ users });
+  const newuser = { register }
+  const users = await collection.insertOne(newuser);
 
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        user: users,
+      },
+      null,
+      2
+    ),
+  };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };

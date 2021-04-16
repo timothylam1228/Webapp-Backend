03f4f50af667bc4156d1995d0bcf1b566976375b
kaking <kakingchow33@gmail.com>
2021-04-16 23:42:06 +0800

    renamed get_item

--- item_handler.js
+++ item_handler.js
@@ -4,21 +4,21 @@ const connectToDB = require('./database')
 const register = require('./controller/register')
 const bcrypt = require('bcryptjs');
 
-module.exports.getitem = async (event) => {
-    const db = await connectToDB.connectToDB();
-    const collection = await db.collection("Product");
-    const seek = await collection.find({}).toArray()
-    console.log(seek)
-    return {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          body:seek
-        },
-        null,
-        2
-      ),
-    };
+module.exports.get_item = async (event) => {
+  const db = await connectToDB.connectToDB();
+  const collection = await db.collection("Product");
+  const seek = await collection.find({}).toArray()
+  console.log(seek)
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        body: seek
+      },
+      null,
+      2
+    ),
+  };
 
-  
-  }
+
+}

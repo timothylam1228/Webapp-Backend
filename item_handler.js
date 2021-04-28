c455d3fe1feb710dd225eb1ace2c3899e77bbc74
Timothy Lam <timothylam1228@gmail.com>
2021-04-29 00:48:05 +0800

    Remove item

--- item_handler.js
+++ item_handler.js
@@ -3,7 +3,7 @@
 const connectToDB = require('./database')
 const register = require('./controller/register')
 const bcrypt = require('bcryptjs');
-
+var ObjectId = require('mongodb').ObjectID;
 module.exports.get_item = async (event) => {
   const db = await connectToDB.connectToDB();
   const collection = await db.collection("Product");
@@ -36,6 +36,26 @@ module.exports.edit_item = async (event) => {
     body: JSON.stringify(
       {
         message: "added"
+      },
+      null,
+      2
+    ),
+  };
+}
+
+module.exports.remove_item = async (event) => {
+  const db = await connectToDB.connectToDB();
+  const collection = await db.collection("Product");
+  const body = JSON.parse(event.body);
+  const id = body._id
+  console.log('id',id)
+
+  collection.deleteOne({ _id :ObjectId(id._id)})
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "removed"
       },
       null,
       2
